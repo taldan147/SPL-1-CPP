ec1603632e84471d547a40742d8c169a8626b6b7
ido <idocarmi2205>
2020-11-04 16:17:19 +0200

    added classes and basic implementation

---
+++ Graph.cpp
@@ -0,0 +1,20 @@
+//
+// Created by spl211 on 04/11/2020.
+//
+#include "Graph.h"
+
+Graph::Graph(std::vector<std::vector<int>> matrix):edges(){
+
+}
+
+Graph::Graph(std::nullptr_t):edges(){
+
+}
+
+void Graph::infectNode(int nodeInd) {
+
+}
+
+bool Graph::isInfected(int nodeInd) {
+    return false;
+}
