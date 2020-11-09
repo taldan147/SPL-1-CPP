a68851076ccd787971cf14bee2974cb296e7bac0
tal <tald96@gmail.com>
2020-11-09 15:53:13 +0200

    impelemnted tree 2.0

--- Tree.h
+++ Tree.h
@@ -2,40 +2,66 @@
 #define TREE_H_
 
 #include <vector>
-
+#include "Session.h"
+#include <queue>
 class Session;
 
 class Tree{
 public:
-    Tree(int rootLabel);
-    void addChild(const Tree& child);
 
+    Tree(int rootLabel); // constructor
+    Tree(const Tree &other); // copy constructor
+    const Tree& operator=(const Tree& other); // copy assignment operator
+    Tree(Tree &&other); // move constructor
+
+
+    const Tree& operator=(Tree&& other); // move assignment operator
+
+    const int & getNode() const;
+    const std::vector<Tree*> & getChildren() const;
+    void addChild(const Tree& child);
+    void addChild(Tree* child);
+    void BFS(const Session &session, const std::vector<std::vector<int>> &edges, Tree* parent,std::vector<bool> visited);
 
     static Tree* createTree(const Session& session, int rootLabel);
     virtual int traceTree()=0;
-private:
+    virtual Tree* clone() const=0;
+    void clearChildren();
+
+
+
+    virtual ~Tree();
+
+protected:
     int node;
     std::vector<Tree*> children;
-}
+};
 
 class CycleTree: public Tree{
 public:
     CycleTree(int rootLabel, int currCycle);
+    CycleTree(const CycleTree& other);
     virtual int traceTree();
+    virtual Tree* clone() const;
 private:
     int currCycle;
-}
+};
 
 class MaxRankTree: public Tree{
 public:
     MaxRankTree(int rootLabel);
     virtual int traceTree();
-}
+    virtual Tree* clone() const;
+
+
+};
 
 class RootTree: public Tree{
 public:
     RootTree(int rootLabel);
+
     virtual int traceTree();
-}
+    virtual Tree* clone() const;
+};
 
 #endif
