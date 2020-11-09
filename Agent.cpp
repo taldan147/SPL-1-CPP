e193ecedee339e925a8de5782c5b0c7ca53c5b4a
ido <idocarmi2205>
2020-11-09 17:31:04 +0200

    added Agent.cpp

---
+++ Agent.cpp
@@ -0,0 +1,25 @@
+//
+// Created by spl211 on 09/11/2020.
+//
+
+#include "Agent.h"
+
+Agent::Agent() {
+
+}
+
+ContactTracer::ContactTracer() {
+
+}
+
+void ContactTracer::act(Session &session) {
+
+}
+
+Virus::Virus(int node) : nodeInd(node) {
+
+}
+
+void Virus::act(Session &session) {
+
+}
